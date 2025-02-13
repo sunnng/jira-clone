d123cca993f5ca239b55f9015465f8ac1ca614d8
Scy <chengyuan7766@gmail.com>
2025-02-13 16:39:00 +0800

    use update workspace hook is done

---
+++ src/features/workspaces/api/use-update-workspace.ts
@@ -0,0 +1,42 @@
+import { InferRequestType, InferResponseType } from "hono";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
+
+import { client } from "@/lib/rpc";
+
+const $patch = client.api.workspaces[":workspaceId"]["$patch"];
+
+type RequestType = InferRequestType<typeof $patch>;
+type ErrorResponseType = { error: string };
+type ResponseType200 = InferResponseType<typeof $patch, 200>;
+
+export const useUpdateWorkspace = () => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<ResponseType200, Error, RequestType>({
+    mutationFn: async ({ form, param }) => {
+      const response = await $patch({ form, param });
+
+      if (response.status === 401) {
+        const errorData = (await response.json()) as ErrorResponseType;
+        throw new Error(`401 Unauthorized:${errorData.error}`);
+      }
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      return await response.json();
+    },
+    onSuccess: ({ data }) => {
+      toast.success("Workspace updated");
+      queryClient.invalidateQueries({ queryKey: ["workspaces"] });
+      queryClient.invalidateQueries({ queryKey: ["workspace", data.$id] });
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+
+  return mutation;
+};
--- src/features/workspaces/schemas.ts
+++ src/features/workspaces/schemas.ts
@@ -9,3 +9,13 @@ export const createWorkspaceSchema = z.object({
     ])
     .optional(),
 });
+
+export const updateWorkspaceSchema = z.object({
+  name: z.string().trim().min(1, "至少一个字符以上").optional(),
+  image: z
+    .union([
+      z.instanceof(File),
+      z.string().transform((value) => (value === "" ? undefined : value)),
+    ])
+    .optional(),
+});
